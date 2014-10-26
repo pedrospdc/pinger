893209a34f088c12ed79b3222c28ad23e136cc49
Pedro Palhares <pedrospdc@gmail.com>
2014-10-25 22:44:05 -0200

    Set elapsed time

--- pinger/workers.py
+++ pinger/workers.py
@@ -10,6 +10,7 @@ from pinger.types import Response, InvalidContent, InvalidStatusCode
 def watcher(url, expected_content, expected_status_code, interval, timeout):
     request_response = requests.get(url, timeout=timeout)
     response = Response(multiprocessing.current_process().name)
+    response.set_elapsed_time = request_response.elapsed
 
     if expected_status_code != request_response.status_code:
         error = InvalidStatusCode(expected_result=expected_status_code, actual_result=request_response.status_code)
@@ -19,6 +20,5 @@ def watcher(url, expected_content, expected_status_code, interval, timeout):
         error = InvalidContent(expected_result=expected_content, actual_result='Content of {}'.format(url))
         response.add_error(error)
 
-    print response
     time.sleep(interval)
     return watcher(url, expected_content, expected_status_code, interval, timeout)
